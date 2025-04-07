4d83f6791ff4c22dc076fc954abc456c44c83c38
FilatovDmitrij <dmitrijfilatov670@gmail.com>
2025-04-08 04:53:43 +0500

    Lab_4.cpp

--- Sem 2/Zel_metod/Lab_4/Lab_4.cpp
+++ Sem 2/Zel_metod/Lab_4/Lab_4.cpp
@@ -1,62 +1,70 @@
 #include <iostream>
 #define MAX_SIZE 100
 
-int main() {
+using namespace std;
+
+int main() 
+{
     int ring[MAX_SIZE];
     int size = 0;
-    
-    std::cout << "Введите количество элементов (не более " << MAX_SIZE << "): ";
-    std::cin >> size;
+   
+    cout << "Введите количество элементов (не более " << MAX_SIZE << "): ";
+    cin >> size;
     
     int i = 0;
-    while(i < size) {
-        std::cout << "Элемент " << i << ": ";
-        std::cin >> ring[i];
+    while(i < size) 
+    {
+        cout << "Элемент " << i << ": ";
+        cin >> ring[i];
         i++;
     }
 
     int K;
-    std::cout << "Введите K для печати влево: ";
-    std::cin >> K;
+    cout << "Введите K для печати влево: ";
+    cin >> K;
     
-    K = (K % size + size) % size; 
+    K = (K % size + size) % size;
     i = K;
-    while(true) {
-        std::cout << ring[i] << " ";
+    while(true) 
+    {
+        cout << ring[i] << " ";
         i = (i - 1 + size) % size;
         if(i == (K - 1 + size) % size) break;
     }
-    std::cout << std::endl;
+    cout << endl;
 
     int max_val = ring[0];
     i = 1;
-    while(i < size) {
+    while(i < size) 
+    {
         if(ring[i] > max_val) max_val = ring[i];
         i++;
     }
     
     int new_size = 0;
     i = 0;
-    while(i < size) {
-        if(ring[i] != max_val) {
+    while(i < size) 
+    {
+        if(ring[i] != max_val) 
+        {
             ring[new_size] = ring[i];
             new_size++;
         }
         i++;
     }
     size = new_size;
-
-    std::cout << "Введите K для печати вправо: ";
-    std::cin >> K;
+    cout << "Введите K для печати вправо: ";
+    cin >> K;
     
-    K = (K % size + size) % size; 
+    K = (K % size + size) % size;
     i = K;
-    while(true) {
-        std::cout << ring[i] << " ";
+    while(true) 
+    {
+        cout << ring[i] << " ";
         i = (i + 1) % size;
         if(i == (K + 1) % size) break;
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
