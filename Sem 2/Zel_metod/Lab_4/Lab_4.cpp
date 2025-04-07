7afa38451bf826a7838c1ed1fecb10869f5691dd
FilatovDmitrij <dmitrijfilatov670@gmail.com>
2025-04-08 03:29:57 +0500

    Lab_4.cpp

--- Sem 2/Zel_metod/Lab_4/Lab_4.cpp
+++ Sem 2/Zel_metod/Lab_4/Lab_4.cpp
@@ -1,78 +1,62 @@
 #include <iostream>
-using namespace std
-int main()
-{
- int a[100], b=1, tmp, n, k, max ;
- cin >> n ;
- for (int i = 0; i<=n-1; i++)
-    {
-     a[i] = b; 
-     b++; 
-    }
- for (int j = 0; j<=n-1; j++)
-    {
-     cout << a[j] <<  ' ';
-    }
- cout << endl;
- cin >> k;
- tmp = 0;
- while (tmp < n)
-	{
-     if (k == 0) 
-	    {
-		 k = n;
-	    }
-	 cout << a[k-1] << ' ';
-	 k--;
-	 tmp++;
-	}
- b = 0;	
- for (int i = 0; i<=n-2; i++)
-    {
-     max = a[i];
-     if (a[i+1] >max)
-        {
-         max = a[i+1];
-        }    
-    }
- for (int i = 0; i<=n-1; i++)
-    {
-     if (a[i]=max)
-        {
-         b++;
-        }      
-    }
- 
- for (int i = 0; i<=n-2; i++)
-    {
-     if (a[i] == max)
-        {
-         for (int j = i; j<=n-2; j++)
-            {
-             a[j] = a[j+1];   
-            }
-        }    
-    }
- while (tmp < n-b)
-	{
-     if (k == 0) 
-	    {
-		 k = n-b;
-	    }
-	 cout << a[k-1] << ' ';
-	 k--;
-	 tmp++;
-	}
- while (tmp < n-b)
-	{
-		if (k == n-b) 
-		   {
-		    k = 0; 
-		   }
-		cout << a[k] << ' ';
-		k++;
-		tmp++;
-	}
+#define MAX_SIZE 100
 
- return 0;
+int main() {
+    int ring[MAX_SIZE];
+    int size = 0;
+    
+    std::cout << "Введите количество элементов (не более " << MAX_SIZE << "): ";
+    std::cin >> size;
+    
+    int i = 0;
+    while(i < size) {
+        std::cout << "Элемент " << i << ": ";
+        std::cin >> ring[i];
+        i++;
+    }
+
+    int K;
+    std::cout << "Введите K для печати влево: ";
+    std::cin >> K;
+    
+    K = (K % size + size) % size; 
+    i = K;
+    while(true) {
+        std::cout << ring[i] << " ";
+        i = (i - 1 + size) % size;
+        if(i == (K - 1 + size) % size) break;
+    }
+    std::cout << std::endl;
+
+    int max_val = ring[0];
+    i = 1;
+    while(i < size) {
+        if(ring[i] > max_val) max_val = ring[i];
+        i++;
+    }
+    
+    int new_size = 0;
+    i = 0;
+    while(i < size) {
+        if(ring[i] != max_val) {
+            ring[new_size] = ring[i];
+            new_size++;
+        }
+        i++;
+    }
+    size = new_size;
+
+    std::cout << "Введите K для печати вправо: ";
+    std::cin >> K;
+    
+    K = (K % size + size) % size; 
+    i = K;
+    while(true) {
+        std::cout << ring[i] << " ";
+        i = (i + 1) % size;
+        if(i == (K + 1) % size) break;
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
