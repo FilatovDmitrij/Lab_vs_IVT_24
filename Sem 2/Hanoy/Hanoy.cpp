fab61ab8e0bb2c42650fb0b61891d93994c2cd44
FilatovDmitrij <dmitrijfilatov670@gmail.com>
2025-04-08 05:21:19 +0500

    Hanoy.cpp

--- Sem 2/Hanoy/Hanoy.cpp
+++ Sem 2/Hanoy/Hanoy.cpp
@@ -1,48 +1,66 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
-
 using namespace std;
 
-int main()
+const int MAX = 10; 
+int t[3][MAX]; 
+int h[3] = {0};  
+
+void show() 
 {
-	setlocale(LC_ALL, "ru");
-	srand(time(NULL));
+    for (int i = 0; i < 3; i++) 
+    {
+        cout << "Ст " << i+1 << ": ";
+        for (int j = 0; j < h[i]; j++) 
+        {
+            cout << t[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "-----" << endl;
+}
 
-	const int len = 10;
-	int arr[len]{};
-	int tmp;
-	int range_min = 0, range_max = 10;
+void move(int n, char from, char to, char tmp) 
+{
+    if (n == 0) return;
+    
+    move(n - 1, from, tmp, to);
+    
+    int f = from - '1';
+    int d = to - '1';
+    
+    int disk = t[f][h[f]-1];
+    h[f]--;
+    
+    t[d][h[d]] = disk;
+    h[d]++;
+    
+    cout << "Двиг " << n << " из " << from << " в " << to << endl;
+    show();
+    
+    move(n - 1, tmp, to, from);
+}
 
-	for (int i = 0; i < len; ++i)
-	{
-		arr[i] = rand() % (range_max - range_min + 1) + range_min;
-	}
+int main() {
+    setlocale(0, "");
+    int n;
 
-	for (int i = 0; i < len; ++i)
-	{
-		cout << arr[i] << ' ';
-	}
-	cout << endl;
+    cout << "Кол-во колец (1-" << MAX << "): ";
+    cin >> n;
+    cout << endl;
 
-	for (int i = 0; i < len-1; ++i)
-	{
-		for (int j = 0; j < (len - 1 - i); ++j) 
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
-			}
-		}
-	}
+    if (n < 1 || n > MAX) {
+        cout << "Неверно!" << endl;
+        return 1;
+    }
 
-	for (int i = 0; i < len; ++i)
-	{
-		cout << arr[i] << ' ';
-	}
-	cout << endl;
+    for (int i = 0; i < n; i++) {
+        t[0][i] = n - i;
+    }
+    h[0] = n;
 
-	return 0;
+    cout << "Начало:" << endl;
+    show();
+
+    move(n, '1', '3', '2');
+    return 0;
 }
